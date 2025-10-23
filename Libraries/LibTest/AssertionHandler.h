05d67ef0d87073b49150011c18caeec98a2b2fc7
Alfredo Mazzinghi <alfredo.mazzinghi@cl.cam.ac.uk>
2025-10-23 23:54:10 +0000

    LibTest/Cheri: Abstract away the jmp_buf type.
    This is now platform-dependent, because we use a different type on FreeBSD.

--- Libraries/LibTest/AssertionHandler.h
+++ Libraries/LibTest/AssertionHandler.h
@@ -13,14 +13,20 @@
 #ifndef AK_OS_WINDOWS
 #    define LIBTEST_SETJMP(env) sigsetjmp(env, 1)
 #    define LIBTEST_LONGJMP siglongjmp
+#    ifdef AK_OS_FREEBSD
+using PlatformJmpBuf = sigjmp_buf;
+#    else
+using PlatformJmpBuf = jmp_buf;
+#    endif
 #else
 #    define LIBTEST_SETJMP(env) setjmp(env)
 #    define LIBTEST_LONGJMP longjmp
+using PlatformJmpBuf = jmp_buf;
 #endif
 
 namespace Test {
 
-jmp_buf& assertion_jump_buffer();
+PlatformJmpBuf& assertion_jump_buffer();
 void set_assertion_jump_validity(bool);
 bool assertion_jump_validity();
 
